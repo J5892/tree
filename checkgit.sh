2b4c22075b4f9c816c08dda83c48504fb1269672
Jordan Sanborn <jordan@brit.co>
2016-11-30 06:02:53 -0800

    autopullaunch

--- checkgit.sh
+++ checkgit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git remote update
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -9,6 +11,9 @@ if [[ $LOCAL = $REMOTE ]]; then
     echo "Up-to-date"
 elif [[ $LOCAL = $BASE ]]; then
     echo "Need to pull"
+    git pull origin master
+    sudo killall python
+    sudo sh patternlaunch.sh
 elif [[ $REMOTE = $BASE ]]; then
     echo "Need to push"
 else
---
+++ patternlaunch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# flashlaunch.sh
+
+cd /home/pi/tree/dotstar
+sudo python pattern.py
