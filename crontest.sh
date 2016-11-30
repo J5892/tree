3e3596cfac0cc56f9bc895170b6af1bb99047f03
Jordan Sanborn <jordan@brit.co>
2016-11-30 06:32:02 -0800

    update cron

--- crontest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# flashlaunch.sh
-
-cd /home/pi/tree/dotstar
-sudo python crontest.py
--- dotstar/crontest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import time
-
-print('sleeping')
-time.sleep(300)
-print('awaken')
